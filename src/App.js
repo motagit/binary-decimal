3b283f80a0780767b77ad99149a6f13a3f8aca9d
motasjw <lucasm.lopes087@gmail.com>
2021-10-07 16:59:43 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [input, setInput] = useState(''); 
+  const[decimal, setDecimal] = useState(0);
+
+  function binToDec() {
+    setDecimal(parseInt(input, 2));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Binary Form</h1>
+        <input value={input} onInput={e => setInput(e.target.value)}/>
+        <button type="button" onClick={() => binToDec()}>Button</button>
+      <h1>Decimal form</h1>
+      <p>{decimal}</p>
     </div>
   );
 }
